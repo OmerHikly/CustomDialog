8aeed49caaa817f53be8ae1f5ffa42e95affa75f
OmerHikly <ah0303@bs.amalnet.k12.il>
2018-11-22 22:39:52 +0200

    2

--- app/src/main/java/com/example/omer4/customizeddialog/MainActivity.java
+++ app/src/main/java/com/example/omer4/customizeddialog/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.omer4.customizeddialog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent g=new Intent(this,Credits.class);
+        startActivity(g);
+        return super.onOptionsItemSelected(item);
     }
 }
